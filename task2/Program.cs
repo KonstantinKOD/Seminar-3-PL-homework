ebe8ee26b206bb61af4737935ee791d7425d60d1
Konsttantin <Ryabovkostya91@icloud.com>
2023-04-10 01:16:42 +0300

    add homework

---
+++ task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, 
+// которая принимает на вход
+// координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Read(string arg)
+        {
+            System.Console.WriteLine($"Вводи давай {arg}");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                System.Console.WriteLine("Нудны циферки, а не буковки. Давай без шуток!");
+            }
+            return number;
+        }
+
+        double lenght()
+        {
+            int x1 = Read("x1");
+            int y1 = Read("y1");
+            int z1 = Read("z1");
+            int x2 = Read("x2");
+            int y2 = Read("y2");
+            int z2 = Read("z2");
+            double len = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2)
+                        + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)), 3);
+            return len;
+        }
+
+        double L = lenght();
+        System.Console.WriteLine($"Координаты точки: {L}, вот так, голубчик!");
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+cube(Read());
+
+int Read()
+{
+    System.Console.Write("Введи число: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        System.Console.WriteLine("Вводи цифры!");
+    }
+    return number;
+}
+
+void cube(int number)
+{
+    int num = 1;
+    int L = Read();
+    while (num <= L)
+    {
+        double i=Math.Pow(num, 3);
+        Console.Write(i+" ");
+        num++;
+    }
+}
